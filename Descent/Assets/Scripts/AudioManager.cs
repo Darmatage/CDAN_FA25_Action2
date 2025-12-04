05c8cf8fd70a77c5d0608b6d802a5b85130f495c
DesignVT <drreilly@massart.edu>
2025-12-03 23:10:33 -0500

    created audio manager
    I still need to hook up music to specific scenes

---
+++ Descent/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour {
+
+        public AudioSource abyss_ambient;
+        public AudioSource abyss_boss1;
+        public AudioSource abyss_boss2;
+        public AudioSource caves_ambient;
+        public AudioSource caves_boss;
+        public AudioSource coral_ambient;
+        public AudioSource coral_boss;
+        public AudioSource river_ambient;
+        public AudioSource main_menu;
+
+
+        private AudioSource theMusic;
+        private static float musicTimeStamp = 0.0f;
+        public float currentTimeStamp;
+
+        void Awake(){
+                //set the music based on the scene
+                if (SceneManager.GetActiveScene().name == "MainMenu") {theMusic = main_menu;} 
+                else if (SceneManager.GetActiveScene().name == "Abyss") {theMusic = abyss_ambient;} 
+                else if (SceneManager.GetActiveScene().name == "Abyss") {theMusic = abyss_boss1;}
+                else if (SceneManager.GetActiveScene().name == "Abyss") {theMusic = abyss_boss2;}
+                else if (SceneManager.GetActiveScene().name == "Caves") {theMusic = caves_ambient;}
+                else if (SceneManager.GetActiveScene().name == "Caves") {theMusic = caves_boss;}
+                else if (SceneManager.GetActiveScene().name == "Coral") {theMusic = coral_ambient;}
+                else if (SceneManager.GetActiveScene().name == "Coral") {theMusic = coral_boss;}
+                else if (SceneManager.GetActiveScene().name == "River") {theMusic = river_ambient;}
+
+                //set the time and play:
+                theMusic.time = musicTimeStamp;
+                theMusic.Play();
+        }
+
+        void Update(){
+               //keep track of timestamp, to auto-call it in the next scene:
+               musicTimeStamp = theMusic.time;
+               currentTimeStamp = theMusic.time;
+        }
+
+//change timestamp (can be called by door code):
+        public void SetTimeStamp(){
+               musicTimeStamp = theMusic.time;
+        }
+}
